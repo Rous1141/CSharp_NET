08182696f1518500919c9848e98c7b69879d3b16
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-01-27 18:00:04 +0700

    Property Initiation

--- NET1702_PRN/Session03-OOP/SchoolManager/StudentV5/Program.cs
+++ NET1702_PRN/Session03-OOP/SchoolManager/StudentV5/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace StudentV5;
+﻿using StudentV5.Entities;
+
+namespace StudentV5;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //There is the another way to new a class
+        Student stu = new Student(); //ctor default
+        //Everything is empty
+
+        //Quick Set Get
+        stu.Name = "Reimu";
+        stu.Age = 20;
+        stu.Email = "Haku@gmail.com";
+        stu.Gpa = 8.9;
+
+        Console.WriteLine($@"Your infos: Name: {stu.Name} | Age: {stu.Age} | GPA: {stu.Gpa}");
+        
+
+        //OR EVEN A BETTER METHOD
+        Student stu2 = new Student() {Name="Sanae", Gpa=9.2, Age=19, Email="kochiya@gmail.com" }; // New Method for GET - SET -> Quick and Clean
+        //Create a class using Property Initiation => Create and Set variable value for object
+        // Not the same as Name Arguement
+        Console.WriteLine($@"Your infos: Name: {stu2.Name} | Age: {stu2.Age} | GPA: {stu2.Gpa}");
+
+        Console.Read();
     }
 }
 
